b2f89a1d5a8e8537de0566208ce43faac5b13ef8
DoriPS13 <palmer.sanderson@gmail.com>
2014-07-27 17:50:49 -0700

    working on styling still

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require('express'),
   app = express();
 
 
-
+app.use(express.static(__dirname + '/public'));
 app.set("view engine", "ejs");
 // Middleware
 app.use(bodyParser.urlencoded());
